f152a352172c1bb5ca4ea45495580c069dddbc6e
Diego Vargas <diegovs0504@gmail.com>
2023-05-08 12:38:40 -0400

    Main Container Responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ export type IAppProps = {
 const App: React.FC<IAppProps> = () => {
   return (
     <>
-      <div className='flex flex-col gap-12 p-12'>
+      <div className='flex flex-col gap-12 p-12 md:p-16 lg:p-24 xl:px-36 xl:py-16'>
         <Header />
         <HiringInfo />
         <Projects />
