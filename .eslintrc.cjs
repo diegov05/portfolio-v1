d1bab531c3598db8cf579c8f31f5c79821531fca
Diego Vargas <diegovs0504@gmail.com>
2023-05-08 11:17:36 -0400

    Initial Commit
    Created Boilerplate Code for App

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  env: { browser: true, es2020: true, node: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+  },
+  overrides: [
+    {
+      // Apply rule override only to files with the following extensions
+      files: ['*.tsx', '*.jsx'],
+      rules: {
+        '@typescript-eslint/ban-types': [
+          'error',
+          {
+            extendDefaults: true,
+            types: {
+              '{}': false,
+            },
+          },
+        ],
+      },
+    },
+  ]
+}
