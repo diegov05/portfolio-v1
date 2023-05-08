090e2245c483beb72057e24e9eae3f645e70c385
Diego Vargas <diegovs0504@gmail.com>
2023-05-08 11:56:20 -0400

    Basic App Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import "./App.css"
+
+import { Header, HiringInfo, Footer, Projects } from './containers';
 
 export type IAppProps = {
 
 }
 
-const App: React.FC<IAppProps> = (props) => {
+const App: React.FC<IAppProps> = () => {
   return (
-    <div>
-
-    </div>
+    <>
+      <div className='p-4'>
+        <Header />
+        <HiringInfo />
+        <Footer />
+        <Projects />
+      </div>
+    </>
   );
 }
 
