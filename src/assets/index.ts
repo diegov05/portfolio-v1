b3edda8667011262cc046a7b2ea58f8076deff7d
Diego Vargas <diegovs0504@gmail.com>
2023-05-08 11:49:48 -0400

    Logo SVGs Import and Export

---
+++ src/assets/index.ts
@@ -0,0 +1,31 @@
+import hero from "./Hero.svg"
+import github from './github.svg'
+import express from './express.svg'
+import figma from './Figma.svg'
+import react from './React.svg'
+import redux from './Redux.svg'
+import git from './git-bash.svg'
+import firebaseLogo from './firebase.svg'
+import mongodb from './mongodb.svg'
+import next from './Next.svg'
+import nodeLogo from './Node.svg'
+import tailwindLogo from './Tailwind.svg'
+import upwork from './Upwork.svg'
+import linkedin from './Linkedin.svg'
+
+export {
+    hero,
+    github,
+    git,
+    linkedin,
+    upwork,
+    tailwindLogo,
+    nodeLogo,
+    next,
+    mongodb,
+    express,
+    react,
+    redux,
+    firebaseLogo,
+    figma
+}
