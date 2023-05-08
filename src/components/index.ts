52f8173d89a23fbf8726c12e1a34f2bcf6c98791
Diego Vargas <diegovs0504@gmail.com>
2023-05-08 11:54:25 -0400

    Export of all Containers and Components

---
+++ src/components/index.ts
@@ -0,0 +1,9 @@
+import { Hero } from "./Hero/Hero";
+import { Slider } from "./Slider/Slider";
+import { Technologies } from "./Technologies/Technologies";
+
+export {
+    Hero,
+    Slider,
+    Technologies
+}
---
+++ src/containers/index.ts
@@ -0,0 +1,11 @@
+import { Footer } from "./Footer/Footer";
+import { Header } from "./Header/Header";
+import { HiringInfo } from "./HiringInfo/HiringInfo";
+import { Projects } from "./Projects/Projects";
+
+export {
+    Footer,
+    Header,
+    HiringInfo,
+    Projects
+}
