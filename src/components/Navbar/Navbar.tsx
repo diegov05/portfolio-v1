83508a5947540d9cdb2a97179964a5850124ef94
Diego Vargas <diegovs0504@gmail.com>
2023-05-11 13:38:03 -0400

    Updated Responsive Rules in NavBar Component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar: React.FC<INavbarProps> = () => {
             <div className='flex flex-row justify-between items-start w-full'>
                 <div className='flex flex-col gap-4 lg:gap-2 xl:gap-2'>
                     <h1 className='text-white font-black text-xs sm:text-base md:text-lg lg:text-lg xl:text-xl'>Diego Vargas</h1>
-                    <h6 className='text-white font-base text-[8px] leading-5 md:text-xs md:leading-6 lg:text-xs lg:leading-5 xl:text-xs xl:leading-5 w-3/4'>Hello! I'm a Full-Stack  web developer in the MERN stack  specialized in 3D designing!</h6>
+                    <h6 className='text-white font-base text-[8px] leading-5 md:text-xs md:leading-6 lg:text-xs lg:leading-5 xl:text-xs xl:leading-5 w-3/4'>Hello! I'm a Full-Stack  web developer in the MERN stack specialized in 3D designing!</h6>
                 </div>
                 <div className='flex flex-row gap-2 sm:gap-2 md:gap-4 lg:gap-6 xl:gap-8'>
                     <a href="">
