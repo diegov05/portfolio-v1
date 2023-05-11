5ac7906ef98686eb30edf787e54bdc6a4ed4a6c1
Diego Vargas <diegovs0504@gmail.com>
2023-05-11 13:38:36 -0400

    Updated Tailwind Configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ export default {
   ],
   theme: {
     extend: {},
+    screens: {
+      'xs': '300px',
+      's': '450px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+      '3xl': '1920px'
+    }
   },
   plugins: [],
 }
