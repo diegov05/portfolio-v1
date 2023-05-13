6a87dffd78739483c6e721002fd46c6cd8005b46
Diego Vargas <diegovs0504@gmail.com>
2023-05-13 11:11:47 -0400

    Created data for Technologies Component

---
+++ src/components/Technologies/data.ts
@@ -0,0 +1,59 @@
+import { react, redux, git, nodeLogo, express, figma, mongodb, next, tailwindLogo } from "../../assets"
+export const technologiesData = [
+    {
+        name: "React",
+        icon: react,
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        link: "https://react.dev/"
+    },
+    {
+        name: "Tailwind",
+        icon: tailwindLogo,
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        link: "https://tailwindcss.com/"
+    },
+    {
+        name: "NextJS",
+        icon: next,
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        link: "https://nextjs.org/",
+        style: true
+    },
+    {
+        name: "MongoDB",
+        icon: mongodb,
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        link: "https://www.mongodb.com/"
+    },
+    {
+        name: "NodeJS",
+        icon: nodeLogo,
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        link: "https://nodejs.org/en"
+    },
+    {
+        name: "ExpressJS",
+        icon: express,
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        link: "https://expressjs.com/",
+        style: true
+    },
+    {
+        name: "Redux",
+        icon: redux,
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        link: "https://redux.js.org/"
+    },
+    {
+        name: "Git",
+        icon: git,
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        link: "https://git-scm.com/"
+    },
+    {
+        name: "Figma",
+        icon: figma,
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        link: "https://www.figma.com/"
+    }
+]
