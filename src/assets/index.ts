8ce7c2cebde533cdc229bda8b730d3ae11844b02
Diego Vargas <diegovs0504@gmail.com>
2023-05-25 21:09:45 -0400

    Imported and Exported New Images

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,33 +1,11 @@
-import hero from "./Hero.svg"
-import github from './github.svg'
-import express from './express.svg'
-import figma from './Figma.svg'
-import react from './React.svg'
-import redux from './Redux.svg'
-import git from './git-bash.svg'
-import firebaseLogo from './firebase.svg'
-import mongodb from './mongodb.svg'
-import next from './Next.svg'
-import nodeLogo from './Node.svg'
-import tailwindLogo from './Tailwind.svg'
-import upwork from './Upwork.svg'
-import linkedin from './Linkedin.svg'
-import topography from './topography.svg'
+import footerImage from "./footerImage.png"
+import gradient from "./gradient.png"
+import hero from './hero.png'
+import phone from './phone.png'
 
-export {
+export default {
+    footerImage,
+    gradient,
     hero,
-    github,
-    git,
-    linkedin,
-    upwork,
-    tailwindLogo,
-    nodeLogo,
-    next,
-    mongodb,
-    express,
-    react,
-    redux,
-    firebaseLogo,
-    figma,
-    topography
+    phone
 }
