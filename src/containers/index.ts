b1e056d93d1ca6988fe65575ab648e7e014861ee
Diego Vargas <diegovs0504@gmail.com>
2023-05-25 21:10:14 -0400

    Deleted HiringInfo Component

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,11 +1,9 @@
 import { Footer } from "./Footer/Footer";
 import { Header } from "./Header/Header";
-import { HiringInfo } from "./HiringInfo/HiringInfo";
 import { Projects } from "./Projects/Projects";
 
 export {
     Footer,
     Header,
-    HiringInfo,
     Projects
 }
