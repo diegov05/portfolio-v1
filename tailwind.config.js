3e7bdcd6fe3787605063bd8d69d8fc6356dda948
Diego Vargas <diegovs0504@gmail.com>
2023-05-25 21:08:17 -0400

    Updated Tailwind Configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "text": "#061328",
+        "bg": "#fff",
+        "button-primary": "linear-gradient(90deg, #68E6E8 3.02%, #1B50EE 100%);",
+        "bg-secondary": "#C6CFDD",
+        "accent-color": "#1B50EE"
+      },
+    },
     screens: {
       'xs': '300px',
       's': '450px',
@@ -13,7 +21,8 @@ export default {
       'lg': '1024px',
       'xl': '1280px',
       '2xl': '1536px',
-      '3xl': '1920px'
+      '3xl': '1920px',
+      '4xl': '3000px'
     }
   },
   plugins: [],
