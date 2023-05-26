ba4ac8136b742acae28fdc6d2299365086ead1be
Diego Vargas <diegovs0504@gmail.com>
2023-05-25 21:11:04 -0400

    Added App Spacing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import "./App.css"
 
-import { Header, HiringInfo, Footer, Projects } from './containers';
+import { Header, Footer, Projects } from './containers';
 
 export type IAppProps = {
 
 }
 
 const App: React.FC<IAppProps> = () => {
+
   return (
-    <div className=''>
+    <div className='flex flex-col gap-24 p-0 m-0'>
       <Header />
-      <HiringInfo />
       <Projects />
       <Footer />
     </div>
