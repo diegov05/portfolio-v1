f22972b32b39ec7e20402a0a33e220999673521d
Diego Vargas <diegovs0504@gmail.com>
2023-05-29 18:31:46 -0400

    Implemented Slider in Mockups Container

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,10 +2,18 @@ import footerImage from "./footerImage.png"
 import gradient from "./gradient.png"
 import hero from './hero.png'
 import phone from './phone.png'
+import phone_alt from './phone3.png'
+import desktop from "./desktop.png"
+import ipad from './iPad.png'
+import laptop from './laptop.png'
 
 export default {
     footerImage,
     gradient,
     hero,
-    phone
+    phone,
+    phone_alt,
+    desktop,
+    ipad,
+    laptop
 }
--- src/containers/Mockups/Mockups.tsx
+++ src/containers/Mockups/Mockups.tsx
@@ -3,7 +3,6 @@ import images from "../../assets"
 import { Swiper, SwiperSlide } from 'swiper/react'
 import "swiper/css"
 
-
 const Mockups: FC = () => {
     return (
         <div className='flex flex-col items-center gap-24 py-10 px-10 sm:max-4xl:px-20'>
@@ -12,9 +11,44 @@ const Mockups: FC = () => {
                 <h1 className='text-text text-2xl sm:max-lg:text-4xl lg:max-4xl:text-6xl font-extrabold'>At Its <span className='gradient-bg-text'>Finest</span></h1>
                 <h2 data-aos="fade-right" data-aos-delay="300" data-aos-anchor-placement="" className='text-accent-color text-base sm:max-lg:text-xl lg:max-4xl:text-2xl font-extrabold'>Scroll to switch device!</h2>
             </div>
-            <div>
-                <img data-aos="fade-up" data-aos-delay="300" src={images.phone} alt="phone" className='w-[30rem]' />
-            </div>
+            <Swiper
+                effect="coverflow"
+                grabCursor={true}
+                centeredSlides={true}
+                loop={true}
+                slidesPerView="auto"
+                coverflowEffect={{
+                    rotate: 0,
+                    stretch: 0,
+                    depth: 100,
+                    modifier: 2.5,
+                }}
+                pagination={{
+                    clickable: true,
+                }}
+                navigation={{
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                }}
+            >
+                <SwiperSlide>
+                    <div className='flex flex-col justify-center items-center w-full'>
+                        <img data-aos="fade-up" data-aos-delay="300" src={images.phone_alt} alt="phone" className='w-[30rem]' />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className='flex flex-col justify-center items-center w-full'>                        <img data-aos="fade-up" data-aos-delay="300" src={images.laptop} alt="phone" className='w-[50rem]' />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className='flex flex-col justify-center items-center w-full'>                        <img data-aos="fade-up" data-aos-delay="300" src={images.ipad} alt="phone" className='w-[40rem]' />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className='flex flex-col justify-center items-center w-full'>                        <img data-aos="fade-up" data-aos-delay="300" src={images.desktop} alt="phone" className='w-[50rem]' />
+                    </div>
+                </SwiperSlide>
+            </Swiper>
         </div >
     )
 }
